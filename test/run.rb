61ae7b0ee82eb91fcb1e1adcf53d93656ed56d20
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-01-20 17:01:50 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@13 0ab7e575-e793-45bd-9359-b48f9dcc1074

--- test/run.rb
+++ test/run.rb
@@ -9,12 +9,22 @@ module Tank::Test
   CVS_ID = '$Id$'
 end
 
+mask = //                       # any match
+if ($0 == __FILE__) then
+  if (mask_pattern = ARGV.shift) then
+    mask = Regexp.compile(mask_pattern)
+  end
+end
+
 test_dir, this_name = File.split(__FILE__)
 Dir.foreach(test_dir) do |test_rb|
   case (test_rb)
   when this_name
     # skip
   when /^test_.*\.rb$/
-    require File.join(test_dir, test_rb)
+    if (test_rb =~ mask) then
+      puts "load #{test_rb}"
+      require File.join(test_dir, test_rb)
+    end
   end
 end
