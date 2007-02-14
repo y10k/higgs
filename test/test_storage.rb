3f799a46aa3a9207b1b9a8f98c46430ebe995972
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-02-14 16:42:51 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@71 0ab7e575-e793-45bd-9359-b48f9dcc1074

--- test/test_storage.rb
+++ test/test_storage.rb
@@ -14,9 +14,30 @@ module Higgs::StorageTest
       init_options({})
       assert_equal(false, @read_only)
       assert_equal(2, @number_of_read_io)
-      assert_equal(Higgs::Index::GDBM_OPEN[:read], @dbm_read_open)
-      assert_equal(Higgs::Index::GDBM_OPEN[:write], @dbm_write_open)
-      assert_instance_of(Higgs::Lock::FineGrainLockManager, @lock_manager)
+      assert_equal(Higgs::Index::GDBM_OPEN[:read], @dbm_read_open)         # higgs/index/gdbm auto-required
+      assert_equal(Higgs::Index::GDBM_OPEN[:write], @dbm_write_open)       # higgs/index/gdbm auto-required
+      assert_instance_of(Higgs::Lock::FineGrainLockManager, @lock_manager) # higgs/lock auto-required
+    end
+
+    def test_init_options_read_only_true
+      init_options(:read_only => true)
+      assert_equal(true, @read_only)
+    end
+
+    def test_init_options_read_only_false
+      init_options(:read_only => false)
+      assert_equal(false, @read_only)
+    end
+
+    def test_init_options_dbm_open
+      init_options(:dbm_open => { :read => :dummy_read_open, :write => :dummy_write_open })
+      assert_equal(:dummy_read_open, @dbm_read_open)
+      assert_equal(:dummy_write_open, @dbm_write_open)
+    end
+
+    def test_init_options_lock_manager
+      init_options(:lock_manager => :dummy_lock_manager)
+      assert_equal(:dummy_lock_manager, @lock_manager)
     end
   end
 end
