90c2ed35f36b44d4c10c5fafd047c9d9b05dc701
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-02-14 23:25:47 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@80 0ab7e575-e793-45bd-9359-b48f9dcc1074

--- lib/higgs/storage.rb
+++ lib/higgs/storage.rb
@@ -37,7 +37,6 @@ module Higgs
       @name = name
       @tar_name = "#{@name}.tar"
       @idx_name = "#{@name}.idx"
-      @commit_log = {}
       init_options(options)
       if (init_io) then
         build_storage_at_first_time
@@ -139,16 +138,75 @@ module Higgs
     end
 
     def write_and_commit(write_list)
-      eoa = @idx_db['EOA'].to_i
-      @w_tar.seek(eoa)
-      @commit_log.clear
-      for key, value in write_list
-        unless (key.kind_of? String) then
-          raise TypeError, "can't convert #{key.class} to String"
+      commit_time = Time.now
+      commit_log = {}
+      committed = false
+
+      begin
+        eoa = @idx_db['EOA'].to_i
+        @w_tar.seek(eoa)
+
+        for key, ope, value in write_list
+          unless (key.kind_of? String) then
+            raise TypeError, "can't convert #{key.class} to String"
+          end
+          case (ope)
+          when :write
+            commit_log['d:' + key] = @w_tar.pos
+            @w_tar.add(key, value, :mtime => commit_time)
+            if (properties = fetch_properties(key)) then
+              properties['hash'] = Digest::SHA512.hexdigest(value)
+            else
+              properties = {
+                'hash' => Digest::SHA512.hexdigest(value),
+                'created_time' => commit_time,
+                'custom_properties' => {}
+              }
+            end
+            commit_log['p:' + key] = @w_tar.pos
+            @w_tar.add(key + '.properties', properties.to_yaml, :mtime => commit_time)
+          when :delete
+            raise NotImplementedError, 'not implemented delete operation'
+          when :update_properties
+            raise NotImplementedError, 'not implemented update_properties operation'
+          else
+            raise "unknown operation: #{ope}"
+          end
         end
-        @commit_log['d:' + key] = @w_tar.pos
-        
+
+        eoa = @w_tar.pos
+        @w_tar.write_EOA
+        @w_tar.fsync
+
+        rollback_log = {}
+        commit_log.each_key do |key|
+          if (pos = @idx_db[key]) then
+            rollback_log[key] = pos.to_i
+          else
+            rollback_log[key] = :new
+          end
+        end
+        @idx_db['rollback'] = Marshal.dump(rollback_log)
+        @idx_db.sync
+
+        commit_log.each_pair do |key, pos|
+          @idx_db[key] = pos.to_i
+        end
+        @idx_db.sync
+
+        @idx_db['EOA'] = eoa.to_s
+        @idx_db.sync
+        @idx_db.delete('rollback')
+        @idx_db.sync
+        committed = true
+      ensure
+        rollback unless committed
       end
+      nil
+    end
+
+    def rollback
+      raise NotImplementedError, 'not implemented rollback'
     end
 
     def shutdown
