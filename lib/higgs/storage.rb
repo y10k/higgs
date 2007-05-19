0560a6d61d1ac0fa8429d1f4cf75f1961e41ba39
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-05-19 05:17:59 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@410 0ab7e575-e793-45bd-9359-b48f9dcc1074

--- lib/higgs/storage.rb
+++ lib/higgs/storage.rb
@@ -114,11 +114,11 @@ module Higgs
           @logger.info("get file lock for write")
         end
 
-        @logger.info format('block format version: %04x', Block::FMT_VERSION)
+        @logger.info format('block format version: 0x%04X', Block::FMT_VERSION)
         @logger.info("block body cksum type: #{Block::BODY_CKSUM_TYPE}")
         @logger.info("index format version: #{Index::MAJOR_VERSION}.#{Index::MINOR_VERSION}")
         @logger.info("storage data cksum type: #{DATA_CKSUM_TYPE}")
-        @logger.info("storage properties cksum type: #{PROPERTIES_CKSUM_BITS}")
+        @logger.info("storage properties cksum type: #{PROPERTIES_CKSUM_TYPE}")
         @logger.info("storage properties cksum bits: #{PROPERTIES_CKSUM_BITS} ")
 
         @logger.info("properties cache type: #{@properties_cache.class}")
