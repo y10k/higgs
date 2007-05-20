3d1f2e85b97d5631610e186d9af1345c4821702b
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-05-20 15:27:09 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@424 0ab7e575-e793-45bd-9359-b48f9dcc1074

---
+++ misc/io_bench/cksum_bench.rb
@@ -0,0 +1,43 @@
+#!/usr/local/bin/ruby
+
+# for dient(1)
+CVS_ID = '$Id$'
+
+require 'benchmark'
+require 'digest/md5'
+require 'digest/rmd160'
+require 'digest/sha1'
+require 'digest/sha2'
+
+count = (ARGV.shift || '10000').to_i
+size = (ARGV.shift || '65536').to_i
+puts "#{$0}: COUNT:#{count}, SIZE:#{size}"
+
+data = 0xFF.chr * size
+
+task_list = [ 8, 16, 32, 64, 128 ].map{|n|
+  [ "String\#sum(#{n}):",
+    proc{
+      count.times do
+	data.sum(n)
+      end
+    }
+  ]
+} + [
+  Digest::MD5, Digest::RMD160,
+  Digest::SHA1, Digest::SHA256, Digest::SHA384, Digest::SHA512
+].map{|d|
+  [ "#{d}:",
+    proc{
+      count.times do
+	d.digest(data)
+      end
+    }
+  ]
+}
+
+Benchmark.bm(task_list.map{|n, t| n.length }.max) do |x|
+  task_list.each do |name, task|
+    x.report(name) { task.call }
+  end
+end
