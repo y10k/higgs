f8ee75d68e452b8efd4c9827e1b1c39bec734521
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-09-09 13:58:34 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@516 0ab7e575-e793-45bd-9359-b48f9dcc1074

--- lib/higgs.rb
+++ lib/higgs.rb
@@ -4,7 +4,35 @@ require 'higgs/dbm'
 require 'higgs/store'
 require 'higgs/version'
 
-# pure ruby transactional storage compatible with unix TAR format
+# = pure ruby transactional storage compatible with unix TAR format
+#
+# Author:: $Author$
+# Date:: $Date$
+# Revision:: $Revision$
+#
+# == main classes
+#
+# [Higgs::Store] storage interface like pstore
+# [Higgs::DBM] storage interface like dbm
+# [Higgs::Utils::BackupManager] online backup utility (body of <tt>higgs_backup</tt>)
+#
+# == storage safety level
+# === case of no backup
+#
+# [REQUIREMENTS] default
+# [NORMAL SHUTDOWN] OK, no recovery
+# [PROCESS ABORT] OK, recovery on read-write open
+# [SYSTEM ABORT (OS abort)] NG, data is <em>NOT</em> consistent
+#
+# === case of online backup
+#
+# [REQUIREMENTS] open with parameters: <tt>jlog_rotate_max => 0</tt>,
+#                <tt>jlog_rotate_service_uri => "druby://localhost:<em>appropriate_port_number</em>"</tt>
+#                and execute <tt>higgs_backup</tt>
+# [NORMAL SHUTDOWN] OK, no recovery
+# [PROCESS ABORT] OK, automatic recovery on read-write open
+# [SYSTEM ABORT (OS abort)] OK, restore from backup and recovery on read-write open
+#
 module Higgs
 end
 
