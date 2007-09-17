356f196d518e271f9aa18bf6f752c26fa0cb2707
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-09-17 15:35:48 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@523 0ab7e575-e793-45bd-9359-b48f9dcc1074

---
+++ sample/dbmtest.rb
@@ -0,0 +1,38 @@
+#!/usr/local/bin/ruby
+
+require 'higgs'
+
+Higgs::DBM.open('test') {|dbm|
+  dbm.transaction{|tx|
+    keys = tx.keys
+    if (keys.length > 0) then
+      for k in keys
+        puts "-"
+        puts "key: #{k}"
+        puts "value: #{tx[k]}"
+        tx.each_property(k) do |name, value|
+          case (name)
+          when Symbol
+            puts "system_property[#{name}]: #{value}"
+          when String
+            puts "custom_property[#{name}], #{value}"
+          else
+            raise "unexpected property name: #{name}"
+          end
+        end
+      end
+    else
+      tx['foobar'] = 'FB'
+      tx.set_property('foobar', 'number', 0)
+      tx['baz'] = 'BZ'
+      tx.set_property('baz', 'number', 1)
+      tx['quux'] = 'QX'
+      tx.set_property('quux', 'number', 2)
+    end
+  }
+}
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
