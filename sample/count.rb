8a935c8fc6c8de7c0aec4fb4cf7754d4f55fe12a
toki <toki@0ab7e575-e793-45bd-9359-b48f9dcc1074>
2007-09-17 14:52:45 +0000

    sample of Higgs::Store
    git-svn-id: svn+ssh://rubyforge.org/var/svn/higgs/higgs/trunk@518 0ab7e575-e793-45bd-9359-b48f9dcc1074

---
+++ sample/count.rb
@@ -0,0 +1,61 @@
+#!/usr/local/bin/ruby
+
+require 'higgs'
+require 'higgs/thread'          # for Higgs::Barrier
+require 'thread'                # for Mutex
+
+num_of_write_threads = (ARGV.shift || '100').to_i
+num_of_count = (ARGV.shift || '100').to_i
+
+Higgs::Store.open('count') {|st|
+  st.transaction(true) {|tx|
+    puts "start - #{Time.now}"
+    p tx[:count]
+    puts ''
+  }
+
+  th_grp = ThreadGroup.new
+  barrier = Higgs::Barrier.new(num_of_write_threads + 2)
+  is_print = true
+
+  num_of_write_threads.times{|i|
+    th_grp.add Thread.new{
+      barrier.wait
+      st.transaction{|tx|
+        tx[:count] = 0 unless (tx.key? :count)
+        num_of_count.times do
+          tx[:count] += 1
+        end
+      }
+    }
+  }
+
+  th_read = Thread.new{
+    barrier.wait
+    while (is_print)
+      st.transaction(true) {|tx|
+        p tx[:count]
+      }
+      sleep(0.1)
+    end
+  }
+
+  barrier.wait
+  for t in th_grp.list
+    t.join
+  end
+
+  is_print = false
+  th_read.join
+
+  st.transaction(true) {|tx|
+    puts ''
+    puts "last - #{Time.now}"
+    p tx[:count]
+  }
+}
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
