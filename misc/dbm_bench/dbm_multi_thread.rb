07f1ba6e766f9656f86296401a12799aa5a0b0af
TOKI Yoshinori <toki@freedom.ne.jp>
2011-05-13 01:00:57 +0900

    delete Higgs::LockManager.

--- misc/dbm_bench/dbm_multi_thread.rb
+++ misc/dbm_bench/dbm_multi_thread.rb
@@ -5,7 +5,6 @@ $:.unshift File.join(File.dirname($0), '..', '..', 'lib')
 
 require 'benchmark'
 require 'higgs/dbm'
-require 'higgs/lock'
 require 'higgs/thread'
 require 'thread'
 
@@ -21,153 +20,60 @@ conf_path = File.join(File.dirname($0), '.strc')
 options = {}
 options.update(Higgs::Storage.load_conf(conf_path)) if (File.exist? conf_path)
 
-[ Higgs::GiantLockManager.new,
-  Higgs::FineGrainLockManager.new
-].each do |lock_manager|
-  puts lock_manager.class
-  options[:lock_manager] = lock_manager
-  Higgs::DBM.open(name, options) {|dbm|
-    dbm.transaction{|tx|
-      tx[:foo] = 'a'
-      thread_count.times do |i|
-        tx[i] = i.to_s
+Higgs::DBM.open(name, options) {|dbm|
+  dbm.transaction{|tx|
+    tx[:foo] = 'a'
+    thread_count.times do |i|
+      tx[i] = i.to_s
+    end
+  }
+
+  Benchmark.bm(16) do |x|
+    x.report('   read') {
+      (thread_count * transaction_count).times do
+        dbm.transaction(true) {|tx|
+          loop_count.times do
+            tx[:foo]
+          end
+        }
       end
     }
 
-    Benchmark.bm(16) do |x|
-      x.report('   read') {
-        (thread_count * transaction_count).times do
-          dbm.transaction(true) {|tx|
-            loop_count.times do
-              tx[:foo]
-            end
-          }
-        end
-      }
-
-      barrier = Higgs::Barrier.new(thread_count + 1)
-      th_grp = ThreadGroup.new
-      thread_count.times do
-        th_grp.add Thread.new{
-          barrier.wait
-          transaction_count.times do
-            dbm.transaction(true) {|tx|
-              loop_count.times do
-                tx[:foo]
-              end
-            }
-          end
-        }
-      end
-
-      x.report('MT read') {
+    barrier = Higgs::Barrier.new(thread_count + 1)
+    th_grp = ThreadGroup.new
+    thread_count.times do
+      th_grp.add Thread.new{
         barrier.wait
-        for th in th_grp.list
-          th.join
-        end
-      }
-
-      x.report('   write') {
-        (thread_count * transaction_count).times do
-          dbm.transaction{|tx|
-            loop_count.times do
-              tx[:foo] = 'a'
-            end
-          }
-        end
-      }
-
-      barrier = Higgs::Barrier.new(thread_count + 1)
-      th_grp = ThreadGroup.new
-      thread_count.times do
-        th_grp.add Thread.new{
-          barrier.wait
-          transaction_count.times do
-            dbm.transaction{|tx|
-              loop_count.times do
-                tx[:foo] = 'a'
-              end
-            }
-          end
-        }
-      end
-
-      x.report('MT write') {
-        barrier.wait
-        for th in th_grp.list
-          th.join
-        end
-      }
-
-      x.report('   sparse write') {
-        thread_count.times do |i|
-          key = i
-          value = i.to_s
-          transaction_count.times do
-            dbm.transaction{|tx|
-              loop_count.times do
-                tx[key] = value
-              end
-            }
-          end
-        end
-      }
-
-      barrier = Higgs::Barrier.new(thread_count + 1)
-      th_grp = ThreadGroup.new
-      thread_count.times do |i|
-        key = i
-        value = i.to_s
-        th_grp.add Thread.new{
-          barrier.wait
-          transaction_count.times do
-            dbm.transaction{|tx|
-              loop_count.times do
-                tx[key] = value
-              end
-            }
-          end
-        }
-      end
-
-      x.report('MT sparse write') {
-        barrier.wait
-        for th in th_grp.list
-          th.join
-        end
-      }
-
-      x.report('   read/write') {
-        ((thread_count - 1) * transaction_count).times do
-          dbm.transaction(true) {|tx|
-            loop_count.times do
-              tx[:foo]
-            end
-          }
-        end
         transaction_count.times do
-          dbm.transaction{|tx|
+          dbm.transaction(true) {|tx|
             loop_count.times do
-              tx[:foo] = 'a'
+              tx[:foo]
             end
           }
         end
       }
+    end
 
-      barrier = Higgs::Barrier.new(thread_count + 1)
-      th_grp = ThreadGroup.new
-      (thread_count - 1).times do
-        th_grp.add Thread.new{
-          barrier.wait
-          transaction_count.times do
-            dbm.transaction(true) {|tx|
-              loop_count.times do
-                tx[:foo]
-              end
-            }
+    x.report('MT read') {
+      barrier.wait
+      for th in th_grp.list
+        th.join
+      end
+    }
+
+    x.report('   write') {
+      (thread_count * transaction_count).times do
+        dbm.transaction{|tx|
+          loop_count.times do
+            tx[:foo] = 'a'
           end
         }
       end
+    }
+
+    barrier = Higgs::Barrier.new(thread_count + 1)
+    th_grp = ThreadGroup.new
+    thread_count.times do
       th_grp.add Thread.new{
         barrier.wait
         transaction_count.times do
@@ -178,18 +84,105 @@ options.update(Higgs::Storage.load_conf(conf_path)) if (File.exist? conf_path)
           }
         end
       }
+    end
 
-      x.report('MT read/write') {
+    x.report('MT write') {
+      barrier.wait
+      for th in th_grp.list
+        th.join
+      end
+    }
+
+    x.report('   sparse write') {
+      thread_count.times do |i|
+        key = i
+        value = i.to_s
+        transaction_count.times do
+          dbm.transaction{|tx|
+            loop_count.times do
+              tx[key] = value
+            end
+          }
+        end
+      end
+    }
+
+    barrier = Higgs::Barrier.new(thread_count + 1)
+    th_grp = ThreadGroup.new
+    thread_count.times do |i|
+      key = i
+      value = i.to_s
+      th_grp.add Thread.new{
         barrier.wait
-        for th in th_grp.list
-          th.join
+        transaction_count.times do
+          dbm.transaction{|tx|
+            loop_count.times do
+              tx[key] = value
+            end
+          }
         end
       }
     end
 
-    puts ''
-  }
-end
+    x.report('MT sparse write') {
+      barrier.wait
+      for th in th_grp.list
+        th.join
+      end
+    }
+
+    x.report('   read/write') {
+      ((thread_count - 1) * transaction_count).times do
+        dbm.transaction(true) {|tx|
+          loop_count.times do
+            tx[:foo]
+          end
+        }
+      end
+      transaction_count.times do
+        dbm.transaction{|tx|
+          loop_count.times do
+            tx[:foo] = 'a'
+          end
+        }
+      end
+    }
+
+    barrier = Higgs::Barrier.new(thread_count + 1)
+    th_grp = ThreadGroup.new
+    (thread_count - 1).times do
+      th_grp.add Thread.new{
+        barrier.wait
+        transaction_count.times do
+          dbm.transaction(true) {|tx|
+            loop_count.times do
+              tx[:foo]
+            end
+          }
+        end
+      }
+    end
+    th_grp.add Thread.new{
+      barrier.wait
+      transaction_count.times do
+        dbm.transaction{|tx|
+          loop_count.times do
+            tx[:foo] = 'a'
+          end
+        }
+      end
+    }
+
+    x.report('MT read/write') {
+      barrier.wait
+      for th in th_grp.list
+        th.join
+      end
+    }
+  end
+
+  puts ''
+}
 
 # Local Variables:
 # mode: Ruby
